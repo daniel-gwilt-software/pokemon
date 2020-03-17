f27fdbf92778d435eab7c6a171f5f8fa2448d9c7
Daniel Gwilt <daniel.gwilt.software@gmail.com>
2020-03-16 22:50:28 -0700

    Add pokemon list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pokemon';
+  pokemon = [];
+
+  constructor(private http: HttpClient) {
+    this.http.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+      .subscribe((data: any) => {
+        for(let i =0; i < data.results.length; i++) {
+          this.pokemon.push({
+            id: i+1,
+            name: data.results[i].name,
+            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+            + (i+1) + '.png'
+          });
+        }
+        console.log(this.pokemon);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatGridListModule} from '@angular/material/grid-list';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +20,16 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatInputModule,
+    MatIconModule,
+    MatGridListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
