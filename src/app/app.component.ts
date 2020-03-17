52751af7b684945d1ad407a162cd1f28ab4eec91
Daniel Gwilt <daniel.gwilt.software@gmail.com>
2020-03-17 09:12:00 -0700

    Add Prettier config and update formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,29 @@
-import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  pokemon = [];
+  pokemon = []
 
   constructor(private http: HttpClient) {
-    this.http.get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+    this.http
+      .get('https://pokeapi.co/api/v2/pokemon/?limit=151')
       .subscribe((data: any) => {
-        for(let i =0; i < data.results.length; i++) {
+        for (let i = 0; i < data.results.length; i++) {
           this.pokemon.push({
-            id: i+1,
+            id: i + 1,
             name: data.results[i].name,
-            image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
-            + (i+1) + '.png'
+            image:
+              'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' +
+              (i + 1) +
+              '.png',
           });
         }
-        console.log(this.pokemon);
-    });
+        console.log(this.pokemon)
+      });
   }
 }
