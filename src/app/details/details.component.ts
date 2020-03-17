a3235111220813ea1298551990eec288280aa1e6
Daniel Gwilt <daniel.gwilt.software@gmail.com>
2020-03-17 11:44:12 -0700

    Get locations from Craft Demo API

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-details',
@@ -12,10 +12,19 @@ export class DetailsComponent implements OnInit {
 
   details;
 
+  private httpOptions;
+
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private http: HttpClient) { }
+    private http: HttpClient) {
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'x-api-key': 'HHko9Fuxf293b3w56zAJ89s3IcO9D5enaEPIg86l'
+        })
+      };
+    }
 
   ngOnInit() {
     this.route.paramMap.pipe(
@@ -25,6 +34,12 @@ export class DetailsComponent implements OnInit {
     ).subscribe(results => {
       this.details = results;
       this.details.name = this.details.name.replace(/^\w/, c => c.toUpperCase());
+
+      this.http.get('https://api.craft-demo.net/pokemon/' + this.details.id,
+          this.httpOptions)
+        .subscribe((data: any) => {
+          this.details.locations = data.locations;
+        });
       console.log('details: ', this.details);
     });
   }
